c1984f87d02594e77d5aa98552923b1288a9bc5b
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2026-06-12 09:19:04 -0300

    Delete 1jsBasico directory

--- 1jsBasico/manipulandoAtributos.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("input")
-
-// Atualiza um atributo 
-input.setAttribute("disabled", true)
-input.setAttribute("type", "file")
-
-// Remove um atributo
-input.removeAttribute("id")
